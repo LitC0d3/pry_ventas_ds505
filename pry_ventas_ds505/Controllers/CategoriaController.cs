2e6bc65fde325ce5af5fe73955735544ab00d922
Alumno-ETI <Alumno-ETI@DESKTOP-F6UCKKO>
2024-05-17 16:38:31 -0500

    Agregar archivos de proyecto.

---
+++ pry_ventas_ds505/Controllers/CategoriaController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using pry_ventas_ds505.Data;
+using pry_ventas_ds505.Models;
+using System.Collections.Generic;
+
+namespace pry_ventas_ds505.Controllers
+{
+    public class CategoriaController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CategoriaController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Categoria> listaCategorias = _context.Categoria;
+
+            return View(listaCategorias);
+        }
+    }
+}
---
+++ pry_ventas_ds505/Controllers/PersonalController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using pry_ventas_ds505.Data;
+using pry_ventas_ds505.Models;
+
+namespace pry_ventas_ds505.Controllers
+{
+    public class PersonalController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PersonalController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Personal> ListarPersonal = _context.Personal;
+
+            return View(ListarPersonal);
+        }
+    }
+}
